9f5b03a736b84889279720145cc5935f67f0652e
wizzoh <86914860+wizzoh@users.noreply.github.com>
2022-03-09 11:46:31 +0100

    Update lyrics.js

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -1,16 +1,24 @@
+const lyricsFinder = require('lyrics-finder');
+
 module.exports = {
     name: 'lyrics',
     aliases: ['testo'],
     description: 'music Command',
 
-    async execute(message, args, cmd, client, Discord) {
-
+    async execute(message, args, cmd, client, Discord, artist, title) {
+/*
         let queue = client.distube.getQueue(message.guild.id);
         let firstSong = queue.songs[0].name;
+        let lyrics = await lyricsFinder(artist, title) || "Not Found!";
         if (!firstSong) {
             return message.reply("Non sto riproducendo nessuna canzone!");
         } else {
           message.channel.send(`${firstSong}`);
         }
+        */
+        
+        let lyrics = await lyricsFinder(artist, title) || "Not Found!";
+        console.log(lyrics);
+        })("poets of fall", "carnival of rust");
     }
 }
